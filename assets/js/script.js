1bd49694b6f2c6cea7ddc58a930f91aaafcab3d2
Benjamin <mellecbenjamin@gmail.com>
2017-09-15 16:31:17 +0200

    Modification style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,62 @@
-/* fonction de clique avec ajout de 1*/
-function add() {
-    score = score + multiplicateur;
-    document.getElementById("affichage").innerHTML = score;
+$(document).ready(function() {
+
+    var minutes = 25;
+    var secondes = 0;
+    var pause = 0;
+    var interval = 0;
+
+    //Affichage par défaut
+    $("#minutes").html(minutes + ":");
+    $("#secondes").html("0" + secondes);
+
+
+    $("#lancer").click(function() {
+
+        //Décompte
+        var interval = setInterval(function() {
+            secondes -= 1;
+            if (secondes < 0) {
+                minutes--;
+                secondes = 59;
+            }
+            if (minutes < 0) {
+                minutes = 0;
+                secondes = 0;
+                interval = 0;
+                alert("Décompte terminé !");
+            }
+
+            $("#secondes").html(secondes);
+            $("#minutes").html(minutes + ":");
+        }, 1000);
+
+
+        //Bouton pour mettre en pause le timer
+        $("#pause").click(function() {
+            clearInterval(interval);
+            secondes;
+            $("#secondes").html(secondes);
+        });
+
+    });
+    //Bouton pour la pause
+    $("#courtePause").click(function() {
+        setInterval;
+        minutes = 5;
+
+        $("#secondes").html(secondes);
+        $("#minutes").html(minutes + ":");
+    });
+
+    //Bouton pour reset le timer
+    $("#reset").click(function() {
+        clearInterval(interval);
+        secondes = 0;
+        minutes = 25;
+
+        $("#secondes").html("0" + secondes);
+        $("#minutes").html(minutes + ":");
+
+    });
+
+});
